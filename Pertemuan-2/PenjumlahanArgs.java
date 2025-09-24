c57a7c19ecec7c49f4b544ed3a9efa4304516eb4
elvrby <alfarabi1074@gmail.com>
2025-09-25 01:57:37 +0700

    v 0.3

---
+++ Pertemuan-2/PenjumlahanArgs.java
@@ -0,0 +1,12 @@
+public class PenjumlahanArgs{
+ public static void main(String[] args){
+ int bilangan1 = Integer.parseInt(args[0]);
+ int bilangan2 = Integer.parseInt(args[1]);
+
+ System.out.print("Hasil penjumlahan : " + (bilangan1 + bilangan2));
+ }
+}
+
+// *Notes
+// Run di terminal:
+// java PenjumlahanArgs 4 5
